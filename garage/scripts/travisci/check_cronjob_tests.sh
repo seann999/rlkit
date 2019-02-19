32ab578f6c864f7bda384aaa12eb95df20451543
Sean No <seann999@gmail.com>
2019-02-20 02:36:56 +0900

    add garage

--- garage/scripts/travisci/check_cronjob_tests.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-nose2 -c setup.cfg -A cron_job
--- garage/tests/fixtures/__init__.py
+++
@@ -1,5 +0,0 @@
-from tests.fixtures.fixtures import DmParameterizedTestCase
-from tests.fixtures.fixtures import TfGraphTestCase
-from tests.fixtures.fixtures import TfTestCase
-
-__all__ = ['DmParameterizedTestCase', 'TfGraphTestCase', 'TfTestCase']

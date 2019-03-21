33538b7702be81785b18d98d8f0014e3882d1b01
Sean No <seann999@gmail.com>
2019-03-21 23:51:15 +0900

    dmux

--- setenv.sh
+++ setenv.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/user/.mujoco/mjpro150/bin
+export PYTHONPATH=$PYTHONPATH:./garage
 export CHAINER_TYPE_CHECK=0
 export OMP_NUM_THREADS=1
 
+echo $@
 $@
